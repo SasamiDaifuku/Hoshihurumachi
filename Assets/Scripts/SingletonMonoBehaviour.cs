18fe86171e5216b2687e19ad66a64b2a41ec993f
SasamiDaifuku <sasamidaifuku@gmail.com>
2023-09-18 11:26:14 +0900

    Audio部分を変更

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance {
+        get {
+            if (_instance == null) {
+                _instance = (T)FindObjectOfType(typeof(T));
+ 
+                if (_instance == null) {
+                    Debug.LogError (typeof(T) + "is nothing");
+                }
+            }
+ 
+            return _instance;
+        }
+    }
+
+}
